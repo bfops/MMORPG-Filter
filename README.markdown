5bcd4af0e0d19cff76626545ef20de225d1879ca
Ben Foppa <eatingstaples@gmail.com>
2011-06-22 22:06:41 -0400

    Initial script.

--- README.markdown
+++ README.markdown
@@ -1,2 +1,7 @@
 This is a script to sort and filter games at [MMORPG.com's gamelist](http://www.mmorpg.com/gamelist.cfm), with more advanced functionality than it currently offers.
 
+Features:
+
+ * Hides games still in development.
+ * Hides games which require payment to play.
+
---
+++ highlight_games.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name            Highlight games
+// @include         http://www.mmorpg.com/gamelist.cfm
+// @include         http://www.mmorpg.com/gamelist.cfm/*
+// ==/UserScript==
+
+if(unsafeWindow)
+{
+    $ = unsafeWindow.$;
+    setInterval = unsafeWindow.setInterval;
+    clearInterval = unsafeWindow.clearInterval;
+    window = unsafeWindow;
+}
+
+function log(msg)
+{
+    if(window.console) window.console.log(msg);
+    if(GM_log) GM_log(msg);
+}
+
+function matchesHideCriteria(game)
+{
+    var cells = game.children("td");
+
+    // If there's something in the "subscription pay" column.
+    var payImgs = cells.filter(".pay.alt").children("img");
+    if(payImgs.length >= 3 && payImgs.eq(2).attr("src").indexOf("blank") == -1)
+        return true;
+
+    var devImgs = cells.filter(".status.name.first").children("img");
+    if(devImgs.length >= 1 && devImgs.eq(0).attr("lt") == "D")
+        return true;
+
+    return false;
+}
+
+function matchesHighlightCriteria(game)
+{
+    return true;
+}
+
+function highlightText(text)
+{
+    text.css("font-weight", "bold");
+}
+
+function handleRow(row)
+{
+    if(matchesHideCriteria(row))
+        row.hide();
+    else if(matchesHighlightCriteria(row))
+        highlightText(row.children("td").eq(1).closest("a"));
+}
+
+function highlightGameList()
+{
+    var games = $("#gamelisttable > tbody > tr");
+
+    for(var i = 0; i < games.length; ++i)
+        handleRow(games.eq(i));
+}
+
+$(document).ready(highlightGameList);
+
